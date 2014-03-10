185bf334826461b18359e63e54262cda4a4bae7a
Valentin <vgulaev@yandex.ru>
2014-03-10 03:14:39 -0400

    up

--- head.php
+++ head.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name='yandex-verification' content='6aa20153161df0f4' />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta debug = "just for pleasure">
+  <title>TEDxTMN</title>
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="/tedxtmn.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+</head>
 <body>
-<p>Document<p>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Cloud9!';
-
-?>
+<header style = "border-top: solid 15px #EB392E;">
+</header>
+<div id = "logo" class="container">
+<span class = "tedxred">TED</span>
+<span class = "tedxx">X</span>
+<span class = "tedxblack">Tyumen</span>
+<p><span style = "color: #ff2b06; font-weight: bold;">x</span><span style = "font-weight: bold;">=independently organized TED event</span></p>
+</div>
+  <div class="navbar navbar-inverse" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button id = "togglebutton" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <!--a class="navbar-brand" href="/">EZsp</a-->
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/">Главная</a></li>
+          <li><a href="/">О TED и TEDx</a></li>
+          <li><a href="/">Прошедшие мероприятия</a></li>
+          <li><a href="/">Партнеры</a></li>
+          <li><a href="/">Команда</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-
 require("head.php");
+?>
+
+<div id = "maincontainer" class="container">
+В июне 2014 встречате - первый TEDxTmn!!!
+</div>
+
+<?php
+require("footer.php");
 ?>
