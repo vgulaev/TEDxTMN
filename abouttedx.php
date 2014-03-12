80a48d185f0ca7fb0687fb699d848840c0cf132d
vgulaev <vgulaev@yandex.ru>
2014-03-12 11:19:21 +0500

    Zander add

--- abouttedx.php
+++ abouttedx.php
@@ -9,6 +9,7 @@ require("head.php");
 <p><a href="http://www.ted.com/talks/richard_st_john_s_8_secrets_of_success">Ричад Джонсон - 8 секретов успеха</a></p>
 <p><a href="http://www.ted.com/talks/derek_sivers_how_to_start_a_movement">Дерек Сильвер - Как создать движение</a></p>
 <p><a href="http://www.ted.com/talks/frans_de_waal_do_animals_have_morals">Франс de Waal - Моральные принципы у животных</a></p>
+<p><a href="http://www.ted.com/talks/benjamin_zander_on_music_and_passion">Бенжамин Зандер - О пробуждающей силе классической музыки</a></p>
 </div>
 
 <?php
