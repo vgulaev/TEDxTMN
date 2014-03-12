12dd22e031bc14b84e291a103b9b9c383825139a
vgulaev <vgulaev@yandex.ru>
2014-03-12 10:45:10 +0500

    Up

--- abouttedx.php
+++ abouttedx.php
@@ -4,7 +4,11 @@ require("head.php");
 ?>
 
 <div id = "maincontainer" class="container">
-TED - это круто
+<p>TED - это круто</p>
+<p>TEDTalks которые нас вдохновляют:</p>
+<p><a href="http://www.ted.com/talks/richard_st_john_s_8_secrets_of_success">Ричад Джонсон - 8 секретов успеха</a></p>
+<p><a href="http://www.ted.com/talks/derek_sivers_how_to_start_a_movement">Дерек Сильвер - Как создать движение</a></p>
+<p><a href="http://www.ted.com/talks/frans_de_waal_do_animals_have_morals">Франс de Waal - Моральные принципы у животных</a></p>
 </div>
 
 <?php
