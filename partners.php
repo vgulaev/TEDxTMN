fa3bc7bf9fc7c6a67760c86cf471cff369c985f8
vgulaev <vgulaev@yandex.ru>
2014-03-18 11:51:46 +0500

    Up

--- partners.php
+++ partners.php
@@ -5,7 +5,7 @@ require("head.php");
 
 <div id = "maincontainer" class="container">
 <ul>
-<li>Фонд сотвори из жизни шедевр</li>
+<li><a href="http://венскийбал72.рф/">Фонд сотвори из жизни шедевр</a></li>
 <li>Студия ИТ решений VG</li>
 <ul>
 </div>
