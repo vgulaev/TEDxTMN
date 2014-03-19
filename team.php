e69c9d8178f4a0cd8f481885e33e6852fba7cfe8
vgulaev <vgulaev@yandex.ru>
2014-03-19 09:54:43 +0500

    Up

--- team.php
+++ team.php
@@ -4,6 +4,21 @@ require("head.php");
 ?>
 
 <div id = "maincontainer" class="container">
+<p style="font-size: 30px;font-weight: bold;">Команда</p>
+<table>
+	<tbody>
+		<tr>
+			<td>
+				<img src="img\team\gulyaev.jpg">
+			</td>
+			<td style="padding-left: 50px;">
+				<p style="font-weight: bold;">ГУЛЯЕВ ВАЛЕНТИН</p>
+				<p>разработка темы, подбор TEDTalks</p>
+				<p><a href="mailto:vgulaev@yandex.ru">vgulaev@yandex.ru</a></p>
+			</td>
+		</tr>
+	</tbody>
+</table>
 <ul>
 <li>Грошева Юлия</li>
 <li>Путильцева Елена</li>
