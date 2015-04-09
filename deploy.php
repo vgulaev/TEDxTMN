d82c85a58360cb5a5c3deddc164040a24c60ae9d
movis08 <d08121992@mail.ru>
2015-04-09 13:20:16 +0500

    up

--- deploy.php
+++ deploy.php
@@ -4,10 +4,10 @@ $mysqli = new mysqli( 'localhost', 'root', '' );
 $mysqli->query("CREATE DATABASE IF NOT EXISTS tedxtura");
 $mysqli->query("CREATE TABLE IF NOT EXISTS userinv (
         id INT NOT NULL AUTO_INCREMENT,
-        surname CHAR(100), 
-        name CHAR(100), 
-        email CHAR(100), 
-        phone CHAR(100)
+        surname VARCHAR(100),
+        name VARCHAR(100),
+        email VARCHAR(100),
+        phone VARCHAR(100)
         ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
         ");
 echo "string";
