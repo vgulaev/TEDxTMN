1c157d47306146e7e1e66b26fab7d5652bf1e9b5
movis08 <d08121992@mail.ru>
2015-04-10 00:45:38 +0500

    Через файл sett

--- deploy.php
+++ deploy.php
@@ -1,5 +1,6 @@
 <?php
-$mysqli = new mysqli( 'localhost', 'root', '' );
+require("sett.php")
+$mysqli = new mysqli( dbs, dbl, dbp );
 
 $mysqli->query("CREATE DATABASE IF NOT EXISTS tedxtura");
 $mysqli->query("CREATE TABLE IF NOT EXISTS userinv (
@@ -7,8 +8,9 @@ $mysqli->query("CREATE TABLE IF NOT EXISTS userinv (
         surname VARCHAR(100),
         name VARCHAR(100),
         email VARCHAR(100),
-        phone VARCHAR(100)
-        ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+        phone VARCHAR(100),
+        PRIMARY KEY (`id`))
+         ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
         ");
 echo "string";
 ?>
