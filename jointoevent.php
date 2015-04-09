3d2ef1dd7c9f058085414fdb51806b1e9242f32f
movis08 <d08121992@mail.ru>
2015-04-09 13:20:45 +0500

    up

--- jointoevent.php
+++ jointoevent.php
@@ -9,37 +9,39 @@ $mysqli = new mysqli( 'localhost', 'root', '' );
 
 <div id = "maincontainer" class="container">
     <div>
-        <table>
-            <tr>
-                <td>Фамилия:</td>
-                <td>
-                    <input id = "surname"/>
-                </td>
-            </tr>
-            <tr>
-                <td>Имя:</td>
-                <td>
+        <form action="eventregister.php" method="post">
+            <table>
+                <tr>
+                    <td>Фамилия:</td>
+                    <td>
+                        <input id = "surname"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Имя:</td>
+                    <td>
                     <input id = "name"/>
-                </td>
-            </tr>
-            <tr>
-                <td>e-mail:</td>
-                <td>
-                    <input id = "email"/>
-                </td>
-            </tr>
-            <tr>
-                <td>телефон:</td>
-                <td>
-                    <input id = "phone"/>
-                </td>
-            </tr>
-            <tr>
-                <td colspan = "2">
-                    <button style = "width: 100%;">Зарегистрироваться</button>
-                </td>
-            </tr>
-        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>e-mail:</td>
+                    <td>
+                        <input id = "email"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>телефон:</td>
+                    <td>
+                        <input id = "phone"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan = "2">
+                        <input type="submit" style = "width: 100%;" value ="Зарегистрироваться"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
     </div>
 </div>
 <?php
